6e53f32e7d9d3ce77b3eb84e10975bb386490d75
alefbispo <Blacksabbath21@>
2022-05-14 00:00:27 -0300

    campo detalhes de produto

---
+++ ecomerce/lib/styles/detail_screen_styles.dart
@@ -0,0 +1,24 @@
+import 'package:ecomerce/appColors/app_colors.dart';
+import 'package:flutter/cupertino.dart';
+
+class DetailScreenStyle {
+  //commpanyTitleStyle
+  static const TextStyle commpanyTitleStyle = TextStyle(
+      color: AppColors.baseBlackColor,
+      fontSize: 18,
+      fontWeight: FontWeight.w600);
+
+  //productModelTitleStyle
+  static const TextStyle productModelTitleStyle = TextStyle(
+    color: AppColors.baseDarkPinkColor,
+  );
+  //productModelTitleStyle
+  static const TextStyle productPricelTitleStyle = TextStyle(
+    color: AppColors.baseDarkPinkColor,
+  );
+  //productModelTitleStyle
+  static const TextStyle productOldPriceTitleStyle = TextStyle(
+    color: AppColors.baseDarkPinkColor,
+    
+  );
+}
